a60b168eba08ffc41b6e101abf5ecf1c29425fd6
homka122 <kombaev02@gmail.com>
2026-06-17 09:02:05 +0300

    Feat: add processor service

---
+++ processor/cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	collector "github.com/homka122/Gomka122/processor/internal/adapter"
+	"github.com/homka122/Gomka122/processor/internal/config"
+	controller "github.com/homka122/Gomka122/processor/internal/controller/grpc"
+	"github.com/homka122/Gomka122/processor/internal/usecase"
+	pbProcessor "github.com/homka122/Gomka122/proto/processor"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	cfg := config.Load()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.Port))
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := grpc.NewClient(cfg.Port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	collector := collector.NewCollector(conn)
+	usecase := usecase.NewRepositoryUseCase(collector)
+	server := controller.NewServer(usecase)
+
+	grpcServer := grpc.NewServer()
+	pbProcessor.RegisterProcessorServiceServer(grpcServer, server)
+
+	log.Printf("grpc listen on %s port", cfg.Port)
+
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
+}
