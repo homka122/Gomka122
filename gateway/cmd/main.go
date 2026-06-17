dafb30d6f8ac8cf6a7d73ecefcde48405b9cb7e7
homka122 <kombaev02@gmail.com>
2026-06-17 07:46:15 +0300

    Feat: add swagger docs

--- gateway/cmd/main.go
+++ gateway/cmd/main.go
@@ -9,9 +9,23 @@ import (
 	"github.com/homka122/Gomka122/gateway/internal/config"
 	controller "github.com/homka122/Gomka122/gateway/internal/controller/http"
 	"github.com/homka122/Gomka122/gateway/internal/usecase"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+//	@title		Gomka122 API
+//	@version	0.1
+
+//	@contact.name	Homka122
+//	@contact.url	t.me/homka122
+//	@contact.email	kombaev02@gmail.com
+
+//	@license.name	MIT
+//	@license.url	https://mit-license.org/
+
+//	@host		localhost:8080
+//	@BasePath	/api/v1
 
 func main() {
 	cfg := config.Load()
@@ -28,7 +42,10 @@ func main() {
 
 	handler := controller.NewHandler(repositoryUseCase)
 
-	http.HandleFunc("/", handler.GetRepository)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/docs/swagger/", httpSwagger.Handler(httpSwagger.URL(fmt.Sprintf("http://localhost:%s/docs/swagger/doc.json", cfg.Port))))
+	mux.HandleFunc("/repo/{owner}/{repo}", handler.GetRepository)
+
 	log.Printf("run server on %s port\n", cfg.Port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), mux))
 }
