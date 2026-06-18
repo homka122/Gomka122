ef6901d4a79c78f9c5388057e46a384c0ff6a9e8
homka122 <kombaev02@gmail.com>
2026-06-18 08:26:00 +0300

    Feat: add github adapter to subcriber

---
+++ subscriber/internal/adapter/github.go
@@ -0,0 +1,27 @@
+package adapter
+
+import (
+	apperror "github.com/homka122/Gomka122/internal/errors"
+	"github.com/homka122/Gomka122/internal/github"
+)
+
+type GithubRepositoryAdapter struct {
+	GithubClient github.Client
+}
+
+func NewGithubRepositoryAdapter(client github.Client) GithubRepositoryAdapter {
+	return GithubRepositoryAdapter{GithubClient: client}
+}
+
+func (gra GithubRepositoryAdapter) IsExistsRepository(owner, repo string) (bool, error) {
+	_, err := gra.GithubClient.GetRepository(owner, repo)
+	if err != nil {
+		if apperror.CodeOf(err) == apperror.CodeNotFound {
+			return true, nil
+		}
+
+		return false, err
+	}
+
+	return false, nil
+}
